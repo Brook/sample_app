a72fb4fef7af262120ee6dcef915c6c1b997ec2a
Brook <azzzz@gmx.net>
2010-12-01 18:37:45 +0000

    finished password machinery

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20101201015623
+# Schema version: 20101201161821
 #
 # Table name: users
 #
@@ -9,6 +9,7 @@
 #  created_at         :datetime
 #  updated_at         :datetime
 #  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
 class User < ActiveRecord::Base
@@ -16,12 +17,43 @@ class User < ActiveRecord::Base
   attr_accessor   :password
   attr_accessible :name, :email, :password, :password_confirmation
   
-  email_regex = /\A[\w+\-.]+@[a-z.\d\-_]+\.[a-z]+\z/i
+  email_regex = /\A[\w+\-.]+@[a-z.\d\-]+\.[a-z]+\z/i
   
   validates :name,  :presence => true, :length => { :maximum => 50 }
   validates :email, :presence => true, :format => { :with => email_regex },
                     :uniqueness => { :case_sensitive => false }
   validates :password, :presence => true, :confirmation => true,
             :length => { :within => 6..40 } 
+
+  before_save :encrypt_password
+  
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+  
+  def self.authenticate(email, submitted_password)
+    user = User.find_by_email(email)
+    return nil  if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+  
+  private
+  
+  def encrypt_password
+    self.salt = make_salt if new_record?
+    self.encrypted_password = encrypt(password)
+  end
+
+  def encrypt(string)
+    secure_hash("#{salt}--#{string}")
+  end
+  
+  def make_salt
+    secure_hash("#{Time.now.utc}--#{password}")
+  end
+  
+  def secure_hash(string)
+    Digest::SHA2.hexdigest(string)
+  end
 
 end
