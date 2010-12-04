14a4bb12c0bf7e27dbe373be5696bda452767073
Brook <azzzz@gmx.net>
2010-12-04 02:25:53 +0000

    micropost validations

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -18,4 +18,7 @@ class Micropost < ActiveRecord::Base
 
   default_scope :order => 'microposts.created_at DESC'
   
+  validates :user_id, :presence => true
+  validates :content, :presence => true, :length => { :maximum => 140 }
+  
 end
